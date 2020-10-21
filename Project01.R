e34092e9698ededb30258f1c51dd4e2dd175aed3
Michel <58578077+mikedamascenod@users.noreply.github.com>
2020-10-21 15:34:00 -0300

    Add files via upload

---
+++ Project01.R
@@ -0,0 +1,11 @@
+#First project in R Studio. This script creates a sequence on numbers from 20 to 50 and also find numbers from 20 to 60and sum from   51 to 91.
+
+print("Sequence of numbers from 20 to 50:")
+print(seq(20,50))
+print("Mean of numbers from 20 to 60:")
+print("Mean(20:60)")
+print("Sum of numbers from 51 to 91:")
+print(sum(51:91))
+
+
+
